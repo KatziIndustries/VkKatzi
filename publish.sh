98e78d12de3f75327009998f95f9df2eae348124
Aaron_LW <aaronl.waechter@gmail.com>
2026-08-21 16:38:45 +0200

    Change the library naming so it follows the standard

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@ mkdir -p build
 
 VERSION="0.1.0"
 
-gcc -shared -fPIC src/* logger.c -o build/libvkkatzi-$VERSION.so -lvulkan
-gcc -shared -fPIC sdl.c -o build/libvkkatzi_sdl3-$VERSION.so -Lbuild -lvkkatzi-$VERSION -lSDL3
+gcc -shared -fPIC src/* logger.c -o build/libvkkatzi.so.$VERSION -lvulkan
+gcc -shared -fPIC sdl.c -o build/libvkkatzi_sdl3.so.$VERSION -Lbuild -lvkkatzi -lSDL3
