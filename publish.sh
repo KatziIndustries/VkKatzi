3346cefbad2fa15a92fcc715ab9c96d4323effa2
Aaron_LW <aaronl.waechter@gmail.com>
2026-08-21 16:29:51 +0200

    fix publish

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@ mkdir -p build
 VERSION="0.1.0"
 
 gcc -shared -fPIC src/* logger.c -o build/libvkkatzi-$VERSION.so -lvulkan
-gcc -shared -fPIC sdl.c -o build/libvkkatzi_sdl3-$VERSION.so -Lbuild -lvkkatzi -lSDL3
+gcc -shared -fPIC sdl.c -o build/libvkkatzi_sdl3-$VERSION.so -Lbuild -lvkkatzi-$VERSION -lSDL3
